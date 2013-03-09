77c788624affbe801136c61219f818b4c44ad8e2
khoa pham <p.khoa89@yahoo.com>
2013-03-08 21:08:33 -0600

    Finished

---
+++ app/models/appointment.rb
@@ -0,0 +1,5 @@
+class Appointment < ActiveRecord::Base
+  attr_accessible :appointment_date, :patient_id, :physician_id
+  belongs_to :physician
+  belongs_to :patient
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assignment4::Application.config.secret_token = '106c1efeadfb3f12890d8e94285c8dcc17a76f3819262299bae351f56eb03f5dc89a5e4531d42cd753a31c85a703989151d3541f2afca74322e10351e39e02fc'
